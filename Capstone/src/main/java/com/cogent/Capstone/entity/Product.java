7bcfd4e693854f97270ab480d049af9614826142
tylertoussaint <tjtoussaint@gmail.com>
2024-05-23 11:48:56 -0400

    Update Product.java

--- Capstone/src/main/java/com/cogent/Capstone/entity/Product.java
+++ Capstone/src/main/java/com/cogent/Capstone/entity/Product.java
@@ -1,6 +1,8 @@
 package com.cogent.Capstone.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -8,18 +10,22 @@ import jakarta.persistence.Table;
 @Table(name="productdetails")
 public class Product {
 @Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
 private String name;
-private int price;
+private double price;
+private String category;
+
 public String getName() {
 	return name;
 }
 public void setName(String name) {
 	this.name = name;
 }
-public int getPrice() {
+public double getPrice() {
 	return price;
 }
-public void setPrice(int price) {
+public void setPrice(double price) {
 	this.price = price;
 }
 
