dc6bbdbacfe4450fbe0fbf8aef90a0517f55b8c8
tylertoussaint <tjtoussaint@gmail.com>
2024-05-23 12:29:39 -0400

    Update Product.java

--- Capstone/src/main/java/com/cogent/Capstone/entity/Product.java
+++ Capstone/src/main/java/com/cogent/Capstone/entity/Product.java
@@ -28,5 +28,17 @@ public double getPrice() {
 public void setPrice(double price) {
 	this.price = price;
 }
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getCategory() {
+	return category;
+}
+public void setCategory(String category) {
+	this.category = category;
+}
 
 }
