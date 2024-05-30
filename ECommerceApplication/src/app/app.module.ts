c4ea881d53e80a50f55e86912a08c0b728445c34
tylertoussaint <tjtoussaint@gmail.com>
2024-05-30 10:25:17 -0400

    edit

--- ECommerceApplication/src/app/app.module.ts
+++ ECommerceApplication/src/app/app.module.ts
@@ -19,9 +19,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { CartComponent } from './cart/cart.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
+import { UserComponent } from './user/user.component';
 @NgModule({
   declarations: [
-    AppComponent,HomeComponent, RegisterComponent,StocksComponent, SalesComponent, NavComponent,CartComponent, WishlistComponent
+    AppComponent,HomeComponent, RegisterComponent,StocksComponent, SalesComponent, NavComponent,CartComponent, WishlistComponent, UserComponent
 
   ],
   imports: [
